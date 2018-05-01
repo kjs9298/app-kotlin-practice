4e72adc87088a19324f001377b3f95a190f8368b
김지수/메세징플랫폼개발팀/NE <zisu.kim@nhnent.com>
2018-05-01 09:03:56 +0900

    Add practice the local function (and extension function)

---
+++ src/localFunction.kt
@@ -0,0 +1,27 @@
+// Local Function : It can reduce duplicate code
+class User(val id: Int, val name: String, val address: String)
+
+fun User.validateBeforeSave() {
+    fun validate(value: String, fieldName: String) {
+        if(value.isEmpty()) {
+            throw IllegalArgumentException("Can't save user ${this.id}: empty $fieldName")
+
+        }
+
+    }
+
+    validate(this.name, "Name")
+    validate(this.address, "Address")
+
+}
+
+fun saveUser(user: User) {
+    user.validateBeforeSave()
+
+}
+
+fun main(arrays: Array<String>) {
+    saveUser(User(1, "Jisu", "Here"))
+    saveUser(User(2, "Jisu", ""))
+
+}
